0b08e415c0ba1f20af6b572084e5b825cee31e35
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-05-24 12:08:38 -0400

    Allow for a Default Outcome (for every combination)

--- src/lib/OutcomeService.ts
+++ src/lib/OutcomeService.ts
@@ -4,6 +4,9 @@ import { GetOutcomeByAnswersQuery } from '../GraphQL/Queries/Outcome.gql';
 
 export const OutcomeService = () => {
   const GetOutcomeByOptionFilter = async (answers: string[]) => {
+    // Add default option to answers array
+    answers.push('OaQrNE9ZZ0aqnmxmHd_nrw');
+
     const { data, error } = await chOneService().query({
       query: GetOutcomeByAnswersQuery,
       variables: { answers: answers },
