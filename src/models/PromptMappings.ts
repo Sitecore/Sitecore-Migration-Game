48036a8d877c3fd3076786ed3bff74d5adc0d0dc
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-05-29 12:37:35 -0400

    Completed prompt mappings to match to CH ONE data

--- src/models/PromptMappings.ts
+++ src/models/PromptMappings.ts
@@ -1,9 +1,9 @@
 export enum PromptMappings {
   platform = 'introduction',
-  xpFeatures = 'OkD7TPGuAUyF9sx10XKeDw',
-  desiredFramework = 'JMKPmvHV9kCBrnJAl2tj_g',
-  securePages = 'FOrwkq99jkeUMjCw2_qgrg',
-  existingFramework = 'JMKPmvHV9kCBrnJAl2tj_g',
-  experienceEdge = 'NwRb2rE69E2SZgiT-QKpcA',
-  xcFeatures = 'jfZsnoIrjkafjIswkp-oNw',
+  xpFeatures = 'xpfeatures',
+  desiredFramework = 'desiredframeworks',
+  securePages = 'securedpages',
+  existingFramework = 'existingframeworks',
+  experienceEdge = 'experienceedge',
+  xcFeatures = 'xcfeatures',
 }
