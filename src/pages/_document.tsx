d1460f9cc9e33389a166f12aef57f3db2b4bf615
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-06-07 14:48:40 -0400

    favicon + Missing Title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Sitecore Migration Game</title>
+        <link rel="icon" href={`/favicon.png`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
