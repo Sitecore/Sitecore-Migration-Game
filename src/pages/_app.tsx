66360de630dc978c504a991a61503902c35c81c7
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-06-08 10:00:49 -0400

    Cleaned up warning message

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,21 @@
 import { MantineProvider } from '@mantine/core';
 import { GameInfoProvider } from 'components/ui';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <GameInfoProvider>
-      <MantineProvider theme={{ colorScheme: 'light' }}>
-        <Component {...pageProps} />
-      </MantineProvider>
-    </GameInfoProvider>
+    <>
+      <Head>
+        <title>Sitecore Migration Game</title>
+        <link rel="icon" href={`/favicon.png`} />
+      </Head>
+      <GameInfoProvider>
+        <MantineProvider theme={{ colorScheme: 'light' }}>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </GameInfoProvider>
+    </>
   );
 };
 
