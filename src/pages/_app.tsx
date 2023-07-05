1b7cfc2d267124ce28a44c3adc5cf6c2bd41aab4
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-07-05 08:07:18 -0700

    Add color shades default to first element

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,11 @@ import Head from 'next/head';
 const corporateTheme: MantineThemeOverride = {
   colorScheme: 'light',
   colors: {
-    sitecoreRed: ['#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F'],
-    sitecoreViolet: ['#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9'],
+    sitecoreRed: ['#eb1f1f','#d41c1c','#bc1919','#a51616','#8d1313','#761010','#5e0c0c','#460909','#2f0606','#170303'],
+    sitecoreViolet: ['#5548d9','#4d41c3','#443aae','#3b3298','#332b82','#2b246d','#221d57','#191641','#110e2b','#080716'],
   },
   primaryColor: 'sitecoreViolet',
+  primaryShade: 0,
   headings: {
     fontFamily: 'Avenir Next LT Pro, Roboto, sans-serif',
     fontWeight: 500,
