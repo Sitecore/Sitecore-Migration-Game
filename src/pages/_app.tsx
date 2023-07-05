e9043b706373d6230be14d82f16783a2b76c2459
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-07-05 07:49:11 -0700

    Create initial corporate theme in Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, MantineThemeOverride } from '@mantine/core';
 import { GameInfoProvider } from 'components/ui';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
+const corporateTheme: MantineThemeOverride = {
+  colorScheme: 'light',
+  colors: {
+    sitecoreRed: ['#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F','#EB1F1F'],
+    sitecoreViolet: ['#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9','#5548D9'],
+  },
+  primaryColor: 'sitecoreViolet',
+  headings: {
+    fontFamily: 'Avenir Next LT Pro, Roboto, sans-serif',
+    fontWeight: 500,
+  },
+  fontFamily: 'Avenir Next LT Pro, Roboto, sans-serif',
+};
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,7 +26,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" href={`/favicon.png`} />
       </Head>
       <GameInfoProvider>
-        <MantineProvider theme={{ colorScheme: 'light' }}>
+        <MantineProvider theme={corporateTheme}>
           <Component {...pageProps} />
         </MantineProvider>
       </GameInfoProvider>
