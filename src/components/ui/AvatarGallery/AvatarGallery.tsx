ed2d9321be7ad2bc892975f03ed7dece8d5617b3
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-07-11 10:47:35 -0400

    Updating gallery to show 4 images per row

--- src/components/ui/AvatarGallery/AvatarGallery.tsx
+++ src/components/ui/AvatarGallery/AvatarGallery.tsx
@@ -13,7 +13,7 @@ export const AvatarGallery: FC<AvatarGalleryProps> = ({ avatars, handleAvatarCha
     <>
       <Box>
         <Title order={3}>Select Your Avatar:</Title>
-        <SimpleGrid mt={10} cols={2} breakpoints={[{ maxWidth: '56rem', cols: 1, spacing: 'sm' }]}>
+        <SimpleGrid mt={10} cols={4} breakpoints={[{ maxWidth: '56rem', cols: 1, spacing: 'sm' }]}>
           {avatars?.map((avatar, i) => (
             <Card withBorder radius="md" p="md" className={classStyles.card} key={i}>
               {avatar !== undefined && (
