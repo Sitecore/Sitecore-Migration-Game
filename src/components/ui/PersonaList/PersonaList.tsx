e8ddab1765617e9861abc035778962620a4302d2
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-07-11 13:06:19 -0400

    Simplifying PersonaList to take up less space and allow for longer titles in a 4-grid.

--- src/components/ui/PersonaList/PersonaList.tsx
+++ src/components/ui/PersonaList/PersonaList.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Group, SimpleGrid, Title } from '@mantine/core';
+import { Box, Button, SimpleGrid, Title } from '@mantine/core';
 import { IPersona } from 'models';
 import { FC } from 'react';
 
@@ -15,13 +15,9 @@ export const PersonaList: FC<PersonaListProps> = ({ personas, handlePersonaChang
         <Title order={3}>Select Your Role:</Title>
         <SimpleGrid mt={10} cols={4} breakpoints={[{ maxWidth: '56rem', cols: 1, spacing: 'sm' }]}>
           {personas?.map((p, i) => (
-            <Card radius="md" p="md" className={classStyles.card} key={i}>
-              <Group mt="xs">
-                <Button radius="md" style={{ flex: 1 }} onClick={() => handlePersonaChange(p.id)}>
-                  {p.name}
-                </Button>
-              </Group>
-            </Card>
+            <Button radius="md" style={{ flex: 1 }} onClick={() => handlePersonaChange(p.id)}>
+              {p.name}
+            </Button>
           ))}
         </SimpleGrid>
       </Box>
