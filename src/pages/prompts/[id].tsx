21fa75441c1339a7addb4800bda9e65b9733abb6
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-07-12 12:15:47 -0400

    changes to prompts

--- src/pages/prompts/[id].tsx
+++ src/pages/prompts/[id].tsx
@@ -1,25 +1,99 @@
-import { Container } from '@mantine/core';
+import {
+  Avatar,
+  Box,
+  Center,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Stack,
+  VStack,
+} from '@chakra-ui/react';
 import { CurrentPrompt } from 'components/Prompts';
-import { Navigation } from 'components/ui';
 import { PromptService } from 'lib/PromptService';
 import { IAnswer, IPrompt } from 'models';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
 interface PromptPageProps {
   prompt: IPrompt | undefined;
+  gameInfoContext: any;
 }
 
 let promptService = PromptService();
 
-const PromptPage: NextPage<PromptPageProps> = ({ prompt }) => {
+const PromptPage: NextPage<PromptPageProps> = ({ prompt, gameInfoContext }) => {
   const answerSelected = (answer: IAnswer) => {};
 
   return (
     <>
-      <Container my="sm" size="sm" className="App">
-        <Navigation showRestartButton={false} />
-        <CurrentPrompt prompt={prompt} answerSelected={answerSelected} />
-      </Container>
+      <Box
+        position="relative"
+        h="calc(100vh - 64px)"
+        w="full"
+        backgroundImage="/corporate/background.jpg"
+        backgroundSize="cover"
+      >
+        <Center>
+          <Container minW="container.lg">
+            <Grid h="100%" w="100%" templateColumns={{ base: '1fr', lg: '1fr 2fr' }} gap={0}>
+              <Flex justify="center" align="center">
+                <GridItem>
+                  <Center>
+                    <Stack direction={{ base: 'row', lg: 'column' }}>
+                      {gameInfoContext.persona?.personaImage?.results !== undefined && (
+                        <VStack>
+                          <Avatar
+                            size="2xl"
+                            src={gameInfoContext?.persona.personaImage!.results[0].fileUrl}
+                            name={gameInfoContext.persona.personaImage!.results[0].fileName ?? ''}
+                          />
+                          <Heading size="lg">{gameInfoContext?.persona.name}</Heading>
+                        </VStack>
+                      )}
+                      <SimpleGrid columns={3} spacing="2px">
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          1
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          2
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          3
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          4
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          5
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          6
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          7
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          8
+                        </Box>
+                        <Box bg="lightgrey" height="50px" width="50px">
+                          9
+                        </Box>
+                      </SimpleGrid>
+                    </Stack>
+                  </Center>
+                </GridItem>
+              </Flex>
+              <Flex justify="center" align="center">
+                <GridItem>
+                  <CurrentPrompt prompt={prompt} answerSelected={answerSelected} />
+                </GridItem>
+              </Flex>
+            </Grid>
+          </Container>
+        </Center>
+      </Box>
     </>
   );
 };
