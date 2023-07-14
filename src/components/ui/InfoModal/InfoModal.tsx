82109938081eef01e827408c1b6a156527cc3d81
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-07-14 17:02:40 -0400

    Initial modal definition and addition to the header

---
+++ src/components/ui/InfoModal/InfoModal.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { FC } from 'react';
+
+interface InfoModalProps {}
+
+export const InfoModal: FC<InfoModalProps> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button onClick={onOpen}>❔</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>This be some Modal Body, oh yes.</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
