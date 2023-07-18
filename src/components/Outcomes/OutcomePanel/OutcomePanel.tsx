2720549ab8980c14d51cdee932124ef37afe700c
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-07-18 10:56:22 -0400

    Initial outcome design

--- src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
+++ src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
@@ -1,7 +1,7 @@
-import { Paper, Stack } from '@mantine/core';
+import { AbsoluteCenter, Avatar, Box, Center, Heading, Stack, VStack } from '@chakra-ui/react';
 import { OutcomeGenerator } from 'components/Outcomes';
 import { PreviousAnswers } from 'components/Prompts';
-import { InfoBar, useGameInfoContext } from 'components/ui';
+import { HexagonCollection, TwoColumnLayout, useGameInfoContext } from 'components/ui';
 import router from 'next/router';
 import { FC } from 'react';
 
@@ -22,12 +22,36 @@ export const OutcomePanel: FC<OutcomePanelProps> = () => {
   }
 
   return (
-    <Paper p="md" shadow="xs" withBorder>
-      <Stack>
-        <InfoBar remainingQuestions={[]} isSolution={true} />
-        <OutcomeGenerator />
-        <PreviousAnswers />
-      </Stack>
-    </Paper>
+    <TwoColumnLayout
+      leftColumn={
+        <Center>
+          <Stack direction={{ base: 'row', lg: 'column' }}>
+            {gameInfoContext.avatar?.fileUrl !== undefined && gameInfoContext?.persona !== undefined && (
+              <VStack mb={8}>
+                <Avatar width="200px" height="200px" src={gameInfoContext.avatar?.fileUrl} name="User Avatar" />
+                <Box
+                  backgroundColor="white"
+                  width="100%"
+                  height="40px"
+                  position="relative"
+                  boxShadow="0 8px 16px 0 rgba(84,88,89,.4)"
+                >
+                  <AbsoluteCenter axis="both">
+                    <Heading size="md">{gameInfoContext?.persona.name}</Heading>
+                  </AbsoluteCenter>
+                </Box>
+              </VStack>
+            )}
+            <HexagonCollection />
+          </Stack>
+        </Center>
+      }
+      rightColumn={
+        <Stack>
+          <OutcomeGenerator />
+          <PreviousAnswers />
+        </Stack>
+      }
+    ></TwoColumnLayout>
   );
 };
