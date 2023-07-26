26e4f06b402b2d072288eb1fad5c8ef3e0a35ba9
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-07-26 14:44:41 -0400

    Fix for Product Bundle text

--- src/components/Outcomes/OutcomeGenerator/OutcomeGenerator.tsx
+++ src/components/Outcomes/OutcomeGenerator/OutcomeGenerator.tsx
@@ -1,8 +1,9 @@
-import { SimpleGrid, Text, Title } from '@mantine/core';
+import { Blockquote, SimpleGrid, Text, Title } from '@mantine/core';
 import { ConditionalResponse } from 'components/Outcomes';
 import { LinkCard, RichTextOutput, YouTubeVideoDisplay, useGameInfoContext } from 'components/ui';
-import { ExperienceEdgeOption, OutcomeConditions } from 'models/OutcomeConditions';
+import { ExperienceEdgeOption, OutcomeConditions, TargetProduct } from 'models/OutcomeConditions';
 import { FC } from 'react';
+import { FiActivity } from 'react-icons/fi';
 
 interface OutcomeGeneratorProps {}
 
@@ -19,9 +20,23 @@ export const OutcomeGenerator: FC<OutcomeGeneratorProps> = () => {
   //Use the OutcomeConditions class for storing all the answers as the conditions we'll use in the logic.
   let outcomeConditions = new OutcomeConditions(gameInfoContext);
 
+  const requiredProducts: TargetProduct[] = outcomeConditions.requiredProducts();
+
   return (
     <>
       <Title order={2}>{gameInfoContext.outcome.title}</Title>
+      <Text>
+        <RichTextOutput content={gameInfoContext.outcome.productsIntro} />
+      </Text>
+      {requiredProducts && requiredProducts.length > 0 && (
+        <Blockquote icon={<FiActivity />}>
+          <ul>
+            {requiredProducts.map((product: TargetProduct) => (
+              <li key={product}>{product}</li>
+            ))}
+          </ul>
+        </Blockquote>
+      )}
 
       <Title order={2}>{gameInfoContext.outcome.videoTitle}</Title>
       <Text>
