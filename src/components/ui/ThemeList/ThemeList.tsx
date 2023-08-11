b63776e9523e47e70b03fc73b0e3307f539b2772
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-08-11 12:58:05 -0700

    Using screen size to determine number of grid columns

--- src/components/ui/ThemeList/ThemeList.tsx
+++ src/components/ui/ThemeList/ThemeList.tsx
@@ -27,54 +27,53 @@ export const ThemeList: FC<ThemeListProps> = ({ themes, handleThemeChange, class
         <Heading variant={'gameTitle'}>Select Your Theme:</Heading>
       </Center>
       <SimpleGrid
-        columns={2}
+        columns={{ base: 1, md: 2 }}
         spacingX="40px"
         spacingY="20px"
         alignContent={'center'}
         justifyContent={'center'}
         paddingBottom={20}
         marginTop={20}
-        minChildWidth='350px'
       >
-          {themes?.map((theme, i) => (
-            <ScaleFade initialScale={0.9} in={true}>
-              <Center>
-                <Flex
-                  maxWidth={'80%'}
-                  bg={useColorModeValue('white', 'gray.800')}
-                  boxShadow={'10px 25px 50px 25px rgba(0, 0, 0, 0.25)'}
-                  rounded={'lg'}
-                  overflow={'hidden'}
-                  key={i}
-                >
-                  <Stack textAlign={'center'} bg={useColorModeValue('white', 'gray.800')}>
-                    {theme.characterImage?.results !== undefined && (
-                      <Image
-                        src={theme.characterImage!.results[0].fileUrl}
-                        alt={theme.characterImage!.results[0].fileName ?? ''}
-                        maxHeight={'300px'}
-                        fit={'cover'}
-                      />
-                    )}
-                    <Box p={6}>
-                      <Center>
-                        <Heading as="h3" size="md" marginBottom={5}>
-                          {theme.name}
-                        </Heading>
-                      </Center>
+        {themes?.map((theme, i) => (
+          <ScaleFade initialScale={0.9} in={true}>
+            <Center>
+              <Flex
+                maxWidth={'80%'}
+                bg={useColorModeValue('white', 'gray.800')}
+                boxShadow={'10px 25px 50px 25px rgba(0, 0, 0, 0.25)'}
+                rounded={'lg'}
+                overflow={'hidden'}
+                key={i}
+              >
+                <Stack textAlign={'center'} bg={useColorModeValue('white', 'gray.800')}>
+                  {theme.characterImage?.results !== undefined && (
+                    <Image
+                      src={theme.characterImage!.results[0].fileUrl}
+                      alt={theme.characterImage!.results[0].fileName ?? ''}
+                      maxHeight={'300px'}
+                      fit={'cover'}
+                    />
+                  )}
+                  <Box p={6}>
+                    <Center>
+                      <Heading as="h3" size="md" marginBottom={5}>
+                        {theme.name}
+                      </Heading>
+                    </Center>
 
-                      <Text fontSize="md" height={'150px'}>
-                        {theme.description}
-                      </Text>
-                      <Button variant={'continue'} onClick={() => handleThemeChange(theme.id)} margin={5} paddingX={10}>
-                        Continue
-                      </Button>
-                    </Box>
-                  </Stack>
-                </Flex>
-              </Center>
-            </ScaleFade>
-          ))}
+                    <Text fontSize="md" height={'150px'}>
+                      {theme.description}
+                    </Text>
+                    <Button variant={'continue'} onClick={() => handleThemeChange(theme.id)} margin={5} paddingX={10}>
+                      Continue
+                    </Button>
+                  </Box>
+                </Stack>
+              </Flex>
+            </Center>
+          </ScaleFade>
+        ))}
       </SimpleGrid>
     </Box>
   </>
