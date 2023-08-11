8cc84267c2f2787aa34770f39c8bc7417a5315a4
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-08-11 11:32:36 -0400

    Adjusted backgrounds to be theme specific

--- src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
+++ src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
@@ -27,6 +27,11 @@ export const OutcomePanel: FC<OutcomePanelProps> = (props) => {
       showProgressBar={props.showProgressBar}
       showResetButton={props.showResetButton}
       showSaveButton={props.showSaveButton}
+      backgroundImage={
+        gameInfoContext.theme?.id == 'a5F4KpHzIkO1Re9iHmJjWA'
+          ? 'https://mms-delivery.sitecorecloud.io/api/media/v2/delivery/df4c80ea-db67-49f8-bcd3-08daadeee4f5/182bc6d196aa465cbf9b614ff2883eb4'
+          : '/corporate/background.jpg'
+      }
       leftColumn={
         <Center>
           <Stack direction={{ base: 'row', lg: 'column' }} gap={{ base: 20, lg: 0 }}>
