b7f0a3298b3af7405dd32d581f7c7e3835c8bf9c
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-08-14 09:42:38 -0700

    Continue button should not cover text on ThemeSelect

--- src/components/ui/ThemeList/ThemeList.tsx
+++ src/components/ui/ThemeList/ThemeList.tsx
@@ -5,11 +5,9 @@ import {
   Flex,
   Heading,
   Image,
-  ScaleFade,
-  SimpleGrid,
   Stack,
   Text,
-  useColorModeValue,
+  useColorModeValue
 } from '@chakra-ui/react';
 import { ITheme } from 'models';
 import { FC } from 'react';
@@ -21,60 +19,50 @@ interface ThemeListProps {
 }
 
 export const ThemeList: FC<ThemeListProps> = ({ themes, handleThemeChange, classStyles }) => (
-  <>
-    <Box>
-      <Center>
-        <Heading variant={'gameTitle'}>Select Your Theme:</Heading>
-      </Center>
-      <SimpleGrid
-        columns={{ base: 1, md: 2 }}
-        spacingX="40px"
-        spacingY="20px"
-        alignContent={'center'}
-        justifyContent={'center'}
-        paddingBottom={20}
-        marginTop={20}
-      >
-        {themes?.map((theme, i) => (
-          <ScaleFade initialScale={0.9} in={true}>
-            <Center>
-              <Flex
-                maxWidth={'80%'}
-                bg={useColorModeValue('white', 'gray.800')}
-                boxShadow={'10px 25px 50px 25px rgba(0, 0, 0, 0.25)'}
+  <Box>
+    <Center>
+      <Heading variant={'gameTitle'}>Select Your Theme:</Heading>
+    </Center>
+    <Flex
+      align="stretch"
+      justifyContent={'center'}
+      direction={{ base: 'column', md: 'row' }}
+      marginTop={20}>
+      {themes?.map((theme, i) => (
+        <Box
+          flex={1}
+          m={4}
+          maxWidth={'80%'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'10px 25px 50px 25px rgba(0, 0, 0, 0.25)'}
+          rounded={'lg'}
+          overflow={'hidden'}
+          key={i} >
+          <Stack textAlign={'center'} justifyContent={'space-between'} height={'100%'} bg={useColorModeValue('white', 'gray.800')}>
+            {theme.characterImage?.results !== undefined && (
+              <Image
+                src={theme.characterImage!.results[0].fileUrl}
+                alt={theme.characterImage!.results[0].fileName ?? ''}
+                maxHeight={'300px'}
+                fit={'cover'}
                 rounded={'lg'}
-                overflow={'hidden'}
-                key={i}
-              >
-                <Stack textAlign={'center'} bg={useColorModeValue('white', 'gray.800')}>
-                  {theme.characterImage?.results !== undefined && (
-                    <Image
-                      src={theme.characterImage!.results[0].fileUrl}
-                      alt={theme.characterImage!.results[0].fileName ?? ''}
-                      maxHeight={'300px'}
-                      fit={'cover'}
-                    />
-                  )}
-                  <Box p={6}>
-                    <Center>
-                      <Heading as="h3" size="md" marginBottom={5}>
-                        {theme.name}
-                      </Heading>
-                    </Center>
-
-                    <Text fontSize="md" height={'150px'}>
-                      {theme.description}
-                    </Text>
-                    <Button variant={'continue'} onClick={() => handleThemeChange(theme.id)} margin={5} paddingX={10}>
-                      Continue
-                    </Button>
-                  </Box>
-                </Stack>
-              </Flex>
+              />
+            )}
+            <Center>
+              <Heading as="h3" size="md" marginBottom={5}>
+                {theme.name}
+              </Heading>
             </Center>
-          </ScaleFade>
-        ))}
-      </SimpleGrid>
-    </Box>
-  </>
+
+            <Text fontSize="md" p={2}>
+              {theme.description}
+            </Text>
+            <Button variant={'continue'} onClick={() => handleThemeChange(theme.id)} margin={5} paddingX={10}>
+              Continue
+            </Button>
+          </Stack>
+        </Box>
+      ))}
+    </Flex>
+  </Box>
 );
