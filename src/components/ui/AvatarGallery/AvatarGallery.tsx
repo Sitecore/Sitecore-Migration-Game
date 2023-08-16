65c4518fcbaa430d0b95daffa0064f5358f60d05
Mark van Aalst <mva@sitecore.net>
2023-08-16 12:55:35 +0200

    responsive role selection

--- src/components/ui/AvatarGallery/AvatarGallery.tsx
+++ src/components/ui/AvatarGallery/AvatarGallery.tsx
@@ -12,13 +12,13 @@ interface AvatarGalleryProps {
 export const AvatarGallery: FC<AvatarGalleryProps> = ({ avatars, toggledAvatarId, handleAvatarChange }) => {
   return (
     <>
-      <Box margin={10}>
+      <Box marginTop={[10]} marginX={[0, 20]}>
         <Center>
           <Heading variant={'gameTitle'} order={3}>
             Select Your Avatar:
           </Heading>
         </Center>
-        <SimpleGrid mt={10} minChildWidth="120px" spacing="20px">
+        <SimpleGrid mt={10} minChildWidth={['60px', '120px']} spacing={['15px', '15px', '20px']}>
           {avatars?.map((avatar, i) => {
             const isToggled = avatar.id === toggledAvatarId;
             return (
@@ -32,8 +32,8 @@ export const AvatarGallery: FC<AvatarGalleryProps> = ({ avatars, toggledAvatarId
                       backgroundSize={'contain'}
                       border={isToggled ? `3px solid #4a28d9` : 'none'}
                       shadow={'md'}
-                      width={100}
-                      height={100}
+                      width={['80px', '80px', '100px']}
+                      height={['80px', '80px', '100px']}
                       isActive={isToggled}
                       variant={isToggled ? 'avatarSelected' : 'avatar'}
                     />
