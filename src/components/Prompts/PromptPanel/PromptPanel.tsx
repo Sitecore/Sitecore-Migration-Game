c08aa92a1661edfc0eca09160be58c7d2d48e2b8
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-08-21 10:19:48 -0400

    background default + fix for if not set

--- src/components/Prompts/PromptPanel/PromptPanel.tsx
+++ src/components/Prompts/PromptPanel/PromptPanel.tsx
@@ -135,7 +135,15 @@ export const PromptPanel: FC<PromptPanelProps> = (props) => {
         </Center>
       }
       rightColumn={<CurrentPrompt prompt={currentPrompt} answerSelected={answerSelected} />}
-      backgroundImage={currentPrompt?.background?.results[0].fileUrl}
+      backgroundImage={
+        gameInfoContext.theme?.chakraTheme == 'corporate' ||
+        currentPrompt?.background?.results[0] === undefined ||
+        currentPrompt.background.results[0].fileUrl === ''
+          ? gameInfoContext.theme?.chakraTheme == 'corporate'
+            ? '/corporate/background.jpg'
+            : 'https://mms-delivery.sitecorecloud.io/api/media/v2/delivery/df4c80ea-db67-49f8-bcd3-08daadeee4f5/182bc6d196aa465cbf9b614ff2883eb4'
+          : currentPrompt?.background?.results[0].fileUrl
+      }
       loading={loading}
     ></TwoColumnLayout>
   );
