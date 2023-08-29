1d035bbd097fa32509f4a5d97253ed0999dc4212
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2023-08-29 10:53:15 -0400

    Shifted to use a react npm package for cookie consent

--- src/components/ui/CookieDisclaimer/CookieDisclaimer.tsx
+++ src/components/ui/CookieDisclaimer/CookieDisclaimer.tsx
@@ -1,33 +1,22 @@
-import { Box, Container, Link, Stack, Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 import { FC } from 'react';
+import CookieConsent from 'react-cookie-consent';
 
 interface CookieDisclaimerProps {}
 
 export const CookieDisclaimer: FC<CookieDisclaimerProps> = (props) => {
   return (
-    <Box borderTopWidth="1px" bgColor="#fff" position="fixed" bottom="0" width="100%">
-      <Container py={{ base: '4', md: '3.5' }} px={4} width="100%" minWidth="full">
-        <Stack
-          direction="row"
-          spacing={{ base: '3', md: '4' }}
-          justify="space-between"
-          align={{ base: 'start', md: 'center' }}
-        >
-          <Box>
-            <Text fontWeight="medium">Privacy Disclaimer</Text>
-            <Text color="fg.muted">
-              We use cookies to provide functionality, to analyze our traffic and to enable social media functionality.
-              By using this website, you agree to the terms of our{' '}
-              <Link href="https://www.sitecore.com/trust/privacy-policy" color="primary" isExternal>
-                Cookie Policy
-              </Link>
-              .
-            </Text>
-          </Box>
-
-          {/* <IconButton icon={<FiX />} variant="tertiary" aria-label="Close banner" /> */}
-        </Stack>
-      </Container>
-    </Box>
+    <CookieConsent
+      location="bottom"
+      style={{ backgroundColor: '#fff', color: '#000' }}
+      buttonStyle={{ backgroundColor: '#5548d9', color: '#fff' }}
+    >
+      We use cookies to provide functionality, to analyze our traffic and to enable social media functionality. By using
+      this website, you agree to the terms of our{' '}
+      <Link href="https://www.sitecore.com/trust/privacy-policy" color="primary" isExternal>
+        Cookie Policy
+      </Link>
+      .
+    </CookieConsent>
   );
 };
