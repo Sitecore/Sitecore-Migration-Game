972919a87afc16ee47dde180630c9c7604d44668
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-10-26 14:38:16 -0700

    Add link to privacy policy and formatting

--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -1,7 +1,8 @@
-import { Button, Box, Checkbox, FormControl, FormLabel, Input, Select, Text, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { Button, Box, Checkbox, FormControl, FormLabel, Input, Select, Text, useColorMode, useColorModeValue, Center, HStack } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import { useForm,} from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { Country, IState, State } from 'country-state-city';
+import Link from 'next/link';
 
 interface IFormInput {
   firstName: string
@@ -43,30 +44,34 @@ export const EmailForm = () => {
 
 
   return (
-    <Box p={2} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg" bgColor={useColorModeValue('white', 'gray.800')}>
+    <Box p={2} maxWidth="800px" borderWidth={1} borderRadius={8} boxShadow="lg" bgColor={useColorModeValue('white', 'gray.800')}>
+      <Center>
+        <Text fontSize='4xl'>Sign up for future content!</Text>
+      </Center>
+      <Text fontSize='xl'>Get this page's details emailed to you and be notified of future migration content.</Text>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl isRequired>
+        <FormControl isRequired pt={4}>
           <FormLabel >First Name</FormLabel>
-          <Input {...register("firstName", { required: true})} />
-          <FormLabel>Last Name</FormLabel>
-          <Input {...register("lastName", { required: true})} />
-          <FormLabel>Email</FormLabel>
-          <Input{...register("email", { required: true})} />
-          <FormLabel>Country Selection</FormLabel>
-          <Select placeholder='Select Country' {...register("country", { required: true})}>
+          <Input {...register("firstName", { required: true })} />
+          <FormLabel pt={4}>Last Name</FormLabel>
+          <Input {...register("lastName", { required: true })} />
+          <FormLabel pt={4}>Email</FormLabel>
+          <Input{...register("email", { required: true })} />
+          <FormLabel pt={4}>Country Selection</FormLabel>
+          <Select placeholder='Select Country' {...register("country", { required: true })}>
             {countryList.map((country) => (
               <option value={country.name}>{country.name}</option>
             ))}
           </Select>
           {watchCountry == "United States" || watchCountry == "Canada" ? (
-            <Select pt={2} placeholder='Select State' {...register("state", { required: true})}>
+            <Select pt={2} placeholder='Select State' {...register("state", { required: true })}>
               {stateList.map((state) => (
                 <option value={state.name}>{state.name}</option>
               ))}
             </Select>
           ) : null}
-          <Text p={4}>By submitting my contact information, I confirm that I have read and agree to the Sitecore Privacy Policy, which explains how Sitecore collects, processes and shares my personal data. I consent to my data being processed in accordance with Sitecore’s Privacy Policy so that Sitecore can optimize my experience with the Sitecore brand.</Text>
-          <Checkbox p={4} {...register("gdprOptIn", { required: true})}>I consent to receive communications about Sitecore’s business in accordance with Sitecore’s Privacy Policy. I understand that I can opt-out at any time.</Checkbox>
+          <Text p={4}>By submitting my contact information, I confirm that I have read and agree to the <Link style={{color: "blue", textDecoration: "underline"}} href="https://www.sitecore.com/trust/privacy-policy" passHref>Sitecore Privacy Policy</Link>, which explains how Sitecore collects, processes and shares my personal data. I consent to my data being processed in accordance with Sitecore’s Privacy Policy so that Sitecore can optimize my experience with the Sitecore brand.</Text>
+          <Checkbox p={4} {...register("gdprOptIn", { required: true })}>I consent to receive communications about Sitecore’s business in accordance with Sitecore’s Privacy Policy. I understand that I can opt-out at any time.</Checkbox>
           <Button
             mt={4}
             colorScheme='teal'
