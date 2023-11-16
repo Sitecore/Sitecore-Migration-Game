e9ed35ec1dee3d6ca8b428b1e656c5a09c9402fb
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-11-16 14:46:51 -0500

    Changing background image on outcome panel

--- src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
+++ src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
@@ -47,7 +47,7 @@ export const OutcomePanel: FC<OutcomePanelProps> = (props) => {
         backgroundImage={
           gameInfoContext.theme?.chakraTheme == 'fantasy'
             ? 'https://mms-delivery.sitecorecloud.io/api/media/v2/delivery/df4c80ea-db67-49f8-bcd3-08daadeee4f5/182bc6d196aa465cbf9b614ff2883eb4'
-            : '/corporate/XM-cloud-3d-logo-hero.jpg'
+            : '/corporate/background.jpg'
         }
         leftColumn={
           <Center>
