08b704dec1401af14ff8a669bc1ddb205c98b3f4
Jason St-Cyr <jst-cyr@users.noreply.github.com>
2023-12-05 17:40:37 -0500

    Replacing background.jpg with hosted image in Content Hub ONE

--- src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
+++ src/components/Outcomes/OutcomePanel/OutcomePanel.tsx
@@ -47,7 +47,7 @@ export const OutcomePanel: FC<OutcomePanelProps> = (props) => {
         backgroundImage={
           gameInfoContext.theme?.chakraTheme == 'fantasy'
             ? 'https://mms-delivery.sitecorecloud.io/api/media/v2/delivery/df4c80ea-db67-49f8-bcd3-08daadeee4f5/182bc6d196aa465cbf9b614ff2883eb4'
-            : '/corporate/background.jpg'
+            : 'https://mms-delivery.sitecorecloud.io/api/media/v2/delivery/df4c80ea-db67-49f8-bcd3-08daadeee4f5/1821f8838e284d6fad1d483d41877aba'
         }
         leftColumn={
           <Center>
