0f7d2efab37b38faab5f1da7dd7050e8fd486534
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2023-12-11 14:39:57 -0800

    Fix title and description meta fields

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,8 +12,8 @@ const Document = () => {
           name="twitter:image"
           content={"https://wwwsitecorecom.azureedge.net/-/media/sitecoresite/images/home/knowledge-center/digital-marketing-resources/migrating-to-xm-cloud/gettyimages-898545250.jpg?mw=310&md=20230901T160358Z&hash=CA7AE4A53096E607A51CA4C172B0E5D8&t=544x310"}
         />
-        <meta name="og:title" content="Find your way to Sitecore's Composable DXP with confidence!"></meta>
-        <meta name="og:description" content="Sitecore Migration Advisor"></meta>
+        <meta name="og:title" content="Sitecore Migration Advisor"></meta>
+        <meta name="og:description" content="Find your way to Sitecore's Composable DXP with confidence!"></meta>
       </Head>
       <body>
         <Main />
